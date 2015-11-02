8981ebbca2281bcc09f37f84cdcdb1834380e30b
Ilya V <wim-norder@ya.ru>
2015-11-02 22:30:50 +0300

    WIP: RSpec

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe Vscale::API::Client.new('2627bf1dc02449d7bba8e4368a521c612b097878df5241be1bb49325471c966f') do
-  pending 'write it'
+describe 'API.Client.new' do
+  pending 'Ok'
 end
